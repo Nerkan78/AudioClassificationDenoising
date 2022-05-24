5fc5f97a5d38d6fc28ea6afb188fe660b0f88d27
Nerkan78 <55361940+Nerkan78@users.noreply.github.com>
2022-05-24 19:00:28 +0300

    Update multiplication.py

--- multiplication.py
+++ multiplication.py
@@ -2,7 +2,6 @@ def multiplicate(A):
     B = [0] * len(A)
     multiplication = 1
     number_of_zeros = 0
-    non_zero_multiplication = 1
     for i, a in enumerate(A):
         if a != 0:
             multiplication *= a
